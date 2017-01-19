1050ab577c20ddc16ca5c1678d8ae438ac6e4848
Aditya <aditya@HS-14048658.local>
2017-01-19 20:55:59 +0530

    repository layer commit

--- src/main/java/in/hopscotch/moments/db/util/JDBCAccess.java
+++ src/main/java/in/hopscotch/moments/db/util/JDBCAccess.java
@@ -1,7 +1,5 @@
 package in.hopscotch.moments.db.util;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
 import javax.sql.DataSource;
--- src/main/java/in/hopscotch/moments/repository/HSMomentsRepository.java
+++ src/main/java/in/hopscotch/moments/repository/HSMomentsRepository.java
@@ -2,10 +2,16 @@ package in.hopscotch.moments.repository;
 
 import java.util.List;
 
+import in.hopscotch.moments.api.response.ChildInfo;
+import in.hopscotch.moments.api.response.DeliveredProducts;
 import in.hopscotch.moments.entity.HSMomentsData;
 
 public interface HSMomentsRepository {
     
     public List<HSMomentsData> getHSMomentsData(boolean newest, int pageNo, int pageSize);
+    
+    public List<DeliveredProducts> getDeliveredProductInfo(Integer customerId);
+    
+    public List<ChildInfo> getChildInfo(Integer customerId);
 
 }
